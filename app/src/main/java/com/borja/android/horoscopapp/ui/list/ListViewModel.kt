369d3060563c72d4b7e8f676fb09e6b88a166a2a
Borchx <borja8bj8@gmail.com>
2023-05-18 18:59:17 +0200

    implemented Dagger Hilt

---
+++ app/src/main/java/com/borja/android/horoscopapp/ui/list/ListViewModel.kt
@@ -0,0 +1,9 @@
+package com.borja.android.horoscopapp.ui.list
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ListViewModel @Inject constructor() :ViewModel() {
+}
